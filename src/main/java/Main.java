e1c9e557801634d17253832cf24857d149587eed
Roman Bulinski <aboandrus@gmail.com>
2019-07-21 19:43:40 +0200

    REfactor code for BigInteger variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,41 @@ public class Main {
 
     public static BigInteger perimeter(BigInteger n) {
 
-        Integer sum = Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
-                .limit(n.longValue()+2L)
-                .map(t -> t[0])
-                .collect(Collectors.summingInt(Integer::intValue));
+//        Integer sum2 = Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]})
+//                .limit(n.longValue()+2L)
+//                .map(t -> t[0])
+//                .collect(Collectors.summingInt(Integer::intValue));
+//        BigInteger sum = new BigInteger();
+//        for(BigInteger i = 0; i<n; i++  ){
 
-        BigInteger bi = BigInteger.valueOf(sum);
+        BigInteger sum = new BigInteger("1");
+        BigInteger counter = new BigInteger("0");
+        BigInteger m = new BigInteger("0");
+        BigInteger mm = new BigInteger("1");
 
-        return bi.multiply( BigInteger.valueOf(4) );
+
+        while( counter.compareTo(n) < 0){
+
+            BigInteger temp;
+
+            temp = mm;
+            mm= m.add(mm);
+            m = temp;
+
+            sum = sum.add(mm);
+
+            BigInteger one = new BigInteger("1");
+            counter = counter.add(one);
+
+//            System.out.println("m : "+m);
+//            System.out.println("mm : "+mm);
+//            System.out.println("sum : "+sum);
+//            System.out.println("couter : "+counter);
+//            System.out.println("----------------------");
+        }
+
+        return sum.multiply( BigInteger.valueOf(4) );
+
     }
 
 
